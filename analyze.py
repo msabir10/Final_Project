aa9e13f698941936ad66a68eb121c2a1a01bf1f2
levflevine <levflevine@gmail.com>
2022-02-13 12:32:54 -0500

    Heroku App

--- analyze.py
+++ analyze.py
@@ -17,6 +17,7 @@ h_database = os.environ['heroku_database'] #bbb.get_key('heroku_database')
 h_user = os.environ['heroku_user'] #bbb.get_key('heroku_user')
 h_password = os.environ['heroku_pass'] #bbb.get_key('heroku_pass')
 heroku_URI = os.environ['heroku_URI']
+h_host = 'ec2-18-235-114-62.compute-1.amazonaws.com'
 
 def data_etl():
     # Read the raw data
--- app.py
+++ app.py
@@ -19,6 +19,7 @@ h_database = os.environ['heroku_database'] #bbb.get_key('heroku_database')
 h_user = os.environ['heroku_user'] #bbb.get_key('heroku_user')
 h_password = os.environ['heroku_pass'] #bbb.get_key('heroku_pass')
 heroku_URI = os.environ['heroku_URI']
+h_host = 'ec2-18-235-114-62.compute-1.amazonaws.com'
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = heroku_URI
